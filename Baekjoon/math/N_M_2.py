b3377e8b50fd33a748edaa2fcd0b9164ea10fac6
suhyukheo <miceman20@gmail.com>
2022-04-15 22:46:48 +0900

    Update

--- Baekjoon/math/N_M_2.py
+++ Baekjoon/math/N_M_2.py
@@ -2,17 +2,16 @@ n,m = list(map(int,input().split()))
  
 s = []
  
-def dfs():
+def dfs(start):
     if len(s)==m:
         print(' '.join(map(str,s)))
         return
     
-    for a in range(1,n+1):
-     s.append(a) 
-     for i in range(1,n+1):
-       if i >s[-1]:
+
+    for i in range(start,n+1):
+      if i not in s:
         s.append(i)
-        dfs()
+        dfs(i+1)
         s.pop()
  
-dfs()
+dfs(1)
