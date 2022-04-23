df88acfa10b7cc5ca1077c7156f295d6a2e82361
suhyukheo <miceman20@gmail.com>
2022-04-23 18:32:40 +0900

    Update

--- Baekjoon/math/최대공약수와 최소공배수.py
+++ Baekjoon/math/최대공약수와 최소공배수.py
@@ -2,9 +2,10 @@ a,b=map(int,input().split(" "))
 if a<b:
   a,b=b,a
 def GCD(a,b):
-  if b==0:
+  if b==0: 
     return a
-  else:
+  else: 
     return GCD(b,a%b)
 gcd=GCD(a,b)
-print(gcd,int((a*b)/gcd))
+print(gcd)
+print(int((a*b)/gcd))
