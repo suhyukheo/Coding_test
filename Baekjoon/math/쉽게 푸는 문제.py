84e1689668dfb3af12a293db37ec591d990786e2
suhyukheo <miceman20@gmail.com>
2022-04-25 23:11:33 +0900

    Update

---
+++ Baekjoon/math/쉽게 푸는 문제.py
@@ -0,0 +1,16 @@
+a,b=map(int,input().split(" "))
+result=[]
+num=0
+for i in range(1,b+1):
+  if num>=b:
+    break
+  c=i
+  for j in range(i):
+    if num>=b:
+     break
+    num+=1
+    result.append(c)
+num=0
+for i in range(a-1,b):
+  num+=result[i]
+print(num)
