6a555742f1664a47f38d55fcc06c336657d71fe8
suhyukheo <miceman20@gmail.com>
2022-04-28 22:22:45 +0900

    Update

--- Baekjoon/Gridy/괄호문제.py
+++ Baekjoon/Gridy/괄호문제.py
@@ -1,17 +1,31 @@
-n=int(input())
-for i in range(n):
-  cnt=0
-  result=input()
-  for j in result:
-    if cnt==-1:
-      break
-    if j=='(':
-      cnt+=1
-    else:
-      cnt-=1
-  if cnt==0:
-    print('Yes')
-  else:
-    print('No')
-      
+# n=int(input())
+# for i in range(n):
+#   cnt=0
+#   result=input()
+#   for j in result:
+#     if cnt==-1:
+#       break
+#     if j=='(':
+#       cnt+=1
+#     else:
+#       cnt-=1
+#   if cnt==0:
+#     print('Yes')
+#   else:
+#     print('No')
   
+from collections import deque
+n=int(input())
+que=deque()
+for i in range(n):
+  que.clear()
+  a=list(input())
+  for i in range(len(a)):
+    if len(que)==0:
+      que.append(a[i])
+    elif len(que)>=1:
+      if a[i]==')':
+        que.pop()
+        print(a)
+      else:
+        que.append(a[i])
