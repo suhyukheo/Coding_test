74f63e4da931a0b644aa8d2b4026ec3a5a28a122
suhyukheo <miceman20@gmail.com>
2022-05-01 17:52:45 +0900

    Update

--- 이코테/Gridy/곱하기혹은더하기.py
+++ 이코테/Gridy/곱하기혹은더하기.py
@@ -1,3 +1,4 @@
+# 현재값(for(1,len(s))에 넣어서 진행) 또는 전체값(s[0]값 넣어서 )이 1보다 작으면 더하기 연산을 아니면 곱하기 연산을 진행
 s=list(map(int,input()))
 result=s[0]
 for i in range(1,len(s)):
@@ -6,4 +7,5 @@ for i in range(1,len(s)):
   else:
     result*=s[i]
 print(result)
+
     
